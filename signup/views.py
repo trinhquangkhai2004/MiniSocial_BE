2921cf5c68f2c53295f5952e9164f4286c68f96c
Minh Hieu <160104466+HieuYeager@users.noreply.github.com>
2026-02-24 12:15:38 +0700

    hoan thanh api login, signup

---
+++ signup/views.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class SignupView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        if User.objects.filter(username=username).exists():
+            return Response({"error": "Username already exists"}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.create_user(username=username, password=password)
+        return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+
